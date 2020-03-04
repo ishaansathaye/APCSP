5176117c254af9743a09b877ade49d18c220132b
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-03-04 10:43:17 -0800

    working with tkinter and guessingGame

--- programs/guessingGame.py
+++ programs/guessingGame.py
@@ -1,3 +1,50 @@
+# from tkinter import *
+#
+# root = Tk()
+# root.title("Computer Guessing Game")
+# root.geometry("500x500")
+# lowVar = StringVar()
+# highVar = StringVar()
+# labelVar = StringVar()
+# guessVar = StringVar()
+#
+# def range():
+#     lowLabel = Label(root, textvariable=lowVar)
+#     lowVar.set("What is the lower bound of the range?: ")
+#     lowLabel.place(x=0, y=10)
+#     text1 = Text(root, height=1.05, width=10)
+#     text1.place(x=250, y=10)
+#
+#     highLabel = Label(root, textvariable=highVar)
+#     highVar.set("What is the higher bound of the range?: ")
+#     highLabel.place(x=0, y=40)
+#     text2 = Text(root, height=1.05, width=10)
+#     text2.place(x=250, y=40)
+#
+#
+# # lBound, hBound = range()
+# # lowBound = str(lBound-1)
+# # highBound = str(hBound)
+#
+# randomLabel = Label(root, textvariable=labelVar)
+# labelVar.set("Computer Guess: ")
+# randomLabel.place(x=150, y=250)
+#
+# randomLabel = Label(root, textvariable=guessVar)
+# guessVar.set("None")
+# randomLabel.place(x=260, y=250)
+#
+# def math()
+#
+#
+# newMatch = True
+# while newMatch:
+#     guessVar.set(text1.get)
+#     root.mainloop()
+
+
+
+
 low = input("What is the lower bound of the range?: ")
 high = input("What is the higher bound of the range?: ")
 print()
